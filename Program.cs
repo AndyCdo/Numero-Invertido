f9fe00d5ef28fb9838863b7effab78b868e7e5ff
Andrea <chela355@hotmail.com>
2021-02-26 18:05:05 -0500

    Se realizo la tarea

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numero_Invertido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero de tres digitos: ");
+            string numero= Console.ReadLine();
+            string numeroInvertido= "";
+            foreach(char letra in numero){
+                numeroInvertido=letra + numeroInvertido;
+
+            }
+            Console.WriteLine("El Numero Invertido es " + numeroInvertido);
+
+        }
+    }
+}
